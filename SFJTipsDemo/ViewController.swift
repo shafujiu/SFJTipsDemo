f932b6a8e2d4e1454223168eccf44dbb53c35bda
Shafujiu <981215707@qq.com>
2020-09-15 00:42:25 +0800

    Initial Commit

---
+++ SFJTipsDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SFJTipsDemo
+//
+//  Created by Shafujiu on 2020/9/15.
+//  Copyright © 2020 Shafujiu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
